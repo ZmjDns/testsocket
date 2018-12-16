411c31caa4242294f30746d9d062b816b4074cbc
ZmjDns <292570927@qq.com>
2018-12-16 22:27:26 +0800

    add change time method

--- app/src/main/java/com/zmj/mvp/testsocket/async/MyHandler.java
+++ app/src/main/java/com/zmj/mvp/testsocket/async/MyHandler.java
@@ -2,6 +2,7 @@ package com.zmj.mvp.testsocket.async;
 
 import android.content.Context;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 
 import java.lang.ref.WeakReference;
@@ -12,6 +13,12 @@ import java.lang.ref.WeakReference;
  */
 public abstract class MyHandler extends Handler {
     private final WeakReference<Context> targetAct;
+
+    /*//HAndler 新的使用方法
+    //1.声明
+    private Handler newHandler;
+    //2.一般在构造方法或初始化的时候实例化
+    //newHandler = new Handler(Looper.getMainLooper());*/
 
     public MyHandler (Context context){
         targetAct = new WeakReference<Context>(context);
