da1bff7f0edc0f8fea520f7b6b8577931f210a53
ZmjDns <292570927@qq.com>
2019-01-09 23:05:10 +0800

    解决support包冲突问题

---
+++ app/src/main/java/com/zmj/mvp/testsocket/async/MyAsyncTask.java
@@ -0,0 +1,15 @@
+package com.zmj.mvp.testsocket.async;
+
+import android.os.AsyncTask;
+import android.text.PrecomputedText;
+
+/**
+ * @author Zmj
+ * @date 2019/1/9
+ */
+public class MyAsyncTask extends AsyncTask<Void,Void,Void> {
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+}
