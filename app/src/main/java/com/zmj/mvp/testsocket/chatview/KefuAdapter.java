0f1e762258d352173d05c4bd54499e5c005a8ab5
ZmjDns <292570927@qq.com>
2019-01-16 22:34:47 +0800

    增加关键字过滤

--- app/src/main/java/com/zmj/mvp/testsocket/chatview/KefuAdapter.java
+++ app/src/main/java/com/zmj/mvp/testsocket/chatview/KefuAdapter.java
@@ -6,10 +6,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SectionIndexer;
 import android.widget.TextView;
 
 import com.zmj.mvp.testsocket.R;
 import com.zmj.mvp.testsocket.bean.User;
+import com.zmj.mvp.testsocket.utils.CharacterParser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +20,7 @@ import java.util.List;
  * @author Zmj
  * @date 2018/11/30
  */
-public class KefuAdapter extends RecyclerView.Adapter<KefuAdapter.KefuViewHolder> {
+public class KefuAdapter extends RecyclerView.Adapter<KefuAdapter.KefuViewHolder> implements SectionIndexer {
 
     private Context context;
     private List<User> userList = new ArrayList<>();
@@ -71,5 +73,32 @@ public class KefuAdapter extends RecyclerView.Adapter<KefuAdapter.KefuViewHolder
             super(itemView);
             tv_kefu = itemView.findViewById(R.id.tv_kefu);
         }
+    }
+
+    @Override
+    public Object[] getSections() {
+        return new Object[0];
+    }
+
+    @Override
+    public int getPositionForSection(int sectionIndex) {
+        for (int i = 0;i < getItemCount(); i++){
+            //可能会报错
+            // 原来是：
+            // setNameLetters(characterParser.getSelling(userObj.optString("nickname")))
+            // kefuUserList.get(i).getNameLetters();
+            String sortStr = CharacterParser.getInstance().getSelling(userList.get(i).getNickname());
+            char firstChar = sortStr.toUpperCase().charAt(0);
+            if (firstChar == sectionIndex){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public int getSectionForPosition(int position) {
+        return 0;
     }
 }
