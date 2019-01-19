5ec057d34c617096530303bf5d866903d7c530ee
ZmjDns <292570927@qq.com>
2019-01-19 21:15:43 +0800

    Retrofit2.0集成

--- app/src/main/java/com/zmj/mvp/testsocket/retrofit/GetRequest_Interface.java
+++ app/src/main/java/com/zmj/mvp/testsocket/retrofit/GetRequest_Interface.java
@@ -1,18 +1,28 @@
 package com.zmj.mvp.testsocket.retrofit;
 
 import com.zmj.mvp.testsocket.bean.ResponseBean;
+import com.zmj.mvp.testsocket.bean.User;
 
+import java.util.Map;
+
+import okhttp3.FormBody;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.HTTP;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
+import retrofit2.http.PartMap;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * @author Zmj
@@ -60,4 +70,91 @@ public interface GetRequest_Interface {
     @POST("/form")
     @Multipart
     Call<ResponseBody> testFileUpload(@Part("name") RequestBody name, @Part("age") RequestBody age, @Part MultipartBody.Part file);
+
+
+
+
+    //详细说明
+    /**
+     * A @Header
+     * 带请求头{@link Header} 和 不固定请求头{@link retrofit2.http.Headers}
+     * 二者效果是一致的，区别在于使用场景和使用方式
+     * @ Header用于添加不固定的请求头，@Headers用于添加固定的请求头
+     * @ Header作用于方法的参数，@headers作用于方法
+     */
+    @GET("user")
+    Call<User> getUser(@Header("Authorization") String authorzation);
+    @GET("user")
+    @Headers("Authrization:authrization")
+    Call<User> getUser();
+
+    /**
+     * B @Body
+     * 作用：以Post方式传递 自定义数据类型 给服务器
+     * 特别注意：如果提交的是一个Map，那么作用相当于@Field
+     * 此时Map需要经过{@link FromBody.Builder } 类处理成符合 OkHttp 格式的表单
+     * FormBody.Builder builder = new FormBody.Builder();
+     * builder.add("key","value");
+     */
+
+    /**
+     * C @Field @FieldMap
+     * 作用：发送Post请求时提交请求的表单字段
+     * 具体使用：与{@link FormUrlEncoded} 注解配合使用
+     */
+    @POST("/form")
+    @FormUrlEncoded
+    Call<ResponseBody> testUrlFormEncoded2(@Field("username")String name,@Field("password")String password);
+    /**
+     * map的key 将作为表单的键  value作为键值
+     * 具体使用：
+     */
+    @POST("/form")
+    @FormUrlEncoded
+    Call<ResponseBody> testUrlFormEncoded3(@FieldMap Map<String,Object> map);
+
+
+    /**
+     * D @Part  @PartMap
+     * 作用：发送Post请求的时候  提交请求的表单字段
+     * 与 @Filed的区别： 功能相同，但携带的参数更加丰富，包括数据流，所以使用于有文件上传的场景
+     * 具体使用：与@Multipart注解配合使用
+     */
+    @POST("/form")
+    @Multipart
+    Call<ResponseBody> testFileUpload1(@Part("name") RequestBody name,@Part("password") RequestBody pass,@Part MultipartBody.Part file);
+    /**
+     * {@link retrofit2.http.PartMap} 注解支持一个map作为参数，支持{@link RequestBody}类型
+     * 如果有其他类型，会被{@link retrofit2.Converter}转换  如（使用{@link com.google.gson.Gson}的{@link retrofit2.converter.gson.GsonRequestBodyConverter}）
+     * 所以{@link MultipartBody.Part} 就不适用了，所以文件只能用 @Part MultipartBody.Part
+     */
+    @POST("/form")
+    @Multipart
+    Call<ResponseBody> testFileUpload2(@PartMap Map<String,RequestBody> args,@Part MultipartBody.Part file);
+
+    @POST
+    @Multipart
+    Call<ResponseBody> testFileUpload3(@PartMap Map<String,RequestBody> args);
+
+
+    /**
+     * E @Query  @QueryMap
+     * 作用：用于@GET方法的查询参数（Query = Url中 '?' 后面的 key-value）
+     * 如：url = http://www.println.net/?cate=android，其中，Query = cate
+     * 具体使用：配置时只需要只需要在接口方法中增加一个参数就行
+     */
+    @GET("/")
+    Call<String> cate(@Query("cate") String cate);
+
+
+    /**
+     * F @Path
+     * 作用：URL地址的缺省值
+     * 具体使用：
+     */
+    @GET("users/{user}/repos")
+    Call<RequestBody> getBlog(@Path("user") String user);
+    //访问的API是：https：//api.github.com/users/{user}/repos
+    //请求时，{user}会被替换为方法的第一个参数
+    //https://blog.csdn.net/carson_ho/article/details/73732076
 }
--- app/src/main/java/com/zmj/mvp/testsocket/retrofit/UseRetrofit.java
+++ app/src/main/java/com/zmj/mvp/testsocket/retrofit/UseRetrofit.java
@@ -2,9 +2,14 @@ package com.zmj.mvp.testsocket.retrofit;
 
 import android.provider.MediaStore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
 import retrofit2.Retrofit;
 
 /**
@@ -14,16 +19,17 @@ import retrofit2.Retrofit;
 public class UseRetrofit {
 
     private Retrofit retrofit;
+    private GetRequest_Interface service;
 
     public UseRetrofit(Retrofit retrofit) {
         this.retrofit = new Retrofit.Builder()
                 .baseUrl("http://192.168.1.254:8080/tadb/")
                 .build();
+        this.service = retrofit.create(GetRequest_Interface.class);
     }
 
-    //不带请求头的
+    //基本使用
     public void multipartUpload(){
-        GetRequest_Interface service = retrofit.create(GetRequest_Interface.class);
         //@FormUrlEncode
         service.testUrlFormEncoded1("aaa","12354");
 
@@ -34,10 +40,52 @@ public class UseRetrofit {
         RequestBody pass = RequestBody.create(textType,"12354");
         RequestBody file = RequestBody.create(MediaType.parse("application/octet-stream"),"这里是模拟文件内容");
 
-        MultipartBody.Part filePart = MultipartBody.Part.createFormData("file","text.txt",file);
 
-        service.testFileUpload(name,pass,filePart);
+        MultipartBody.Part filePart = MultipartBody.Part.createFormData("file","text.txt",file);
+        //发送请求之后获取结果
+        Call<ResponseBody> call = service.testFileUpload(name,pass,filePart);
     }
+
+    //FormUrlEncoded 中的map封装提交
+    public void formUrlEmcoded(){
+        //@Field
+        Call<ResponseBody> call = service.testUrlFormEncoded2("aaaa","12354");
+
+        //@FieldMap
+        Map<String,Object> map = new HashMap<>();
+        map.put("name","aaaa");
+        map.put("password","12354");
+
+        Call<ResponseBody> call1 = service.testUrlFormEncoded3(map);
+    }
+
+    //@part 和@PartMap的使用
+    public void partAndpartMap(){
+        MediaType textType = MediaType.parse("text/plain");
+        RequestBody name = RequestBody.create(textType,"aaa");
+        RequestBody pass = RequestBody.create(textType,"12345");
+        RequestBody file = RequestBody.create(MediaType.parse("application/octet-stream"),"这里是测试内容");
+
+        //@Part
+        MultipartBody.Part filePart = MultipartBody.Part.createFormData("file","test.txt",file);
+        Call<ResponseBody> call = service.testFileUpload1(name,pass,filePart);
+
+        //@PartMap
+        //实现与上面同样的效果
+        Map<String,RequestBody> fileUpload2Args = new HashMap<>();
+        fileUpload2Args.put("name",name);
+        fileUpload2Args.put("password",pass);
+
+        //这里并不会被当成文件，因为没有文件名（包含在Content-Disposition请求头中），但上面的filePart中有
+        //fileUpload2Args.put("file",file);
+
+        Call<ResponseBody> call1 = service.testFileUpload2(fileUpload2Args,filePart);//文件需要单独处理(MultipartBody.Part)
+
+
+
+    }
+
+
 
 
 
